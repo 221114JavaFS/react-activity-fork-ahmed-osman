06f5ed8df51a4cdc6bd18c436c3ef37da648d7f4
Ahmed Osman <ahmedosman36610@gmail.com>
2023-02-28 15:10:38 -0800

    remove functionality

--- src/components/PokemonBox/PokemonBox.tsx
+++ src/components/PokemonBox/PokemonBox.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Pokemon } from '../../models/Pokemon'
+import React from "react";
+import { Pokemon } from "../../models/Pokemon";
 
-export default function PokemonBox(props:Pokemon) {
+type Props = {
+  name: string;
+  level: number;
+  health: number;
+  damage: number;
+  img: string;
+  onRemove: Function;
+};
+
+export default function PokemonBox(props: Props) {
   return (
-         <div>
-            <img src={props.img}/>
-            <h4>{props.name}</h4>
-            <p>Level: {props.level}</p>
-            <p>Health: {props.health}</p>
-            <p>Damage: {props.damage}</p>
-         </div>
-         )
+    <div>
+      <img src={props.img} />
+      <h4>{props.name}</h4>
+      <p>Level: {props.level}</p>
+      <p>Health: {props.health}</p>
+      <p>Damage: {props.damage}</p>
+      <button onClick={() => props.onRemove(props.name)}>Delete</button>
+    </div>
+  );
 }
